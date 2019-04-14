bd14596e7bc9765befe06ccd9e2a3c999b080fc9
Ivan Shashero <ishashero@hystax.com>
2019-04-14 21:52:06 +0300

    Log troubled line in db script + fixes in CSV

--- csv_to_mongo.py
+++ csv_to_mongo.py
@@ -1,15 +1,23 @@
+#!/usr/bin/env python
 import csv
+import logging
 
 from pymongo import MongoClient
 
+LOG = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 client = MongoClient('mongodb://mongo:27017')
 
 docs = []
 
 with open('solubility_product.csv', newline='') as csvfile:
     reader = csv.reader(csvfile)
-    for row in reader:
-        compound, cations, anions, dissotiation, ksp, logpr, comment, color, link = row
+    for i, row in enumerate(reader):
+        try:
+            compound, cations, anions, dissotiation, ksp, logpr, comment, color, link = row
+        except ValueError:
+            LOG.info('troubles with the line %s', i)
+            raise
 
         cations = [x.strip() for x in cations.split(',')]
         anions = [x.strip() for x in anions.split(',')]
